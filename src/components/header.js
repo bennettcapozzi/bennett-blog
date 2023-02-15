cf2feb6c7a3e0bc319d25e4b8fd08fc57b418dc1
Bennett Capozzi <bennett.capozzi@gmail.com>
2023-02-15 09:35:33 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,11 @@ import { Link } from "gatsby"
 export default function Header({ children }) {
   return (
     <div style={{ margin: "auto", paddingTop: "24px", paddingBottom: "40px" }}>
-      Under Construction. You can find me on Substack
-      <Link
-        to="www.bennettcapozzi.substack.com"
+      Under Construction. You can find me on Substack 
+      <script src="www.bennettcapozzi.substack.com"
       >
         here
-      </Link>
+</script>
       {children}
     </div>
   )
